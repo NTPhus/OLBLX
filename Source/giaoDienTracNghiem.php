7a6f54f48fd3abf8ed48fba0959445330969c532
NTPhus <phu444080@gmail.com>
2024-04-20 22:01:20 +0700

    Change a little bit about css

--- Source/giaoDienTracNghiem.php
+++ Source/giaoDienTracNghiem.php
@@ -12,7 +12,7 @@
     <div class="item1">
         <?php
             for($i = 1; $i <= 100; $i++){
-                echo "<button name='cau' class="."btn"." value=".$i.">".$i."</button>";
+                echo "<button name='cau' class="."btn"." value=".$i." id="."btn".$i.">".$i."</button>";
             }
         ?>
     </div>
@@ -47,10 +47,7 @@
         $img = $data["img"];
     }
 
-    mysqli_close($conn);
     ?>
-
-    
         <div>
             <span>
                 <p id="cauHoi"> <?php echo $cauhoifull."<br>"; ?></p>
@@ -59,14 +56,13 @@
         <?php
         
         if($img == 1) echo "<img src='"."Anh/Câu ".$cau.".png'".">"."<br>";
-        if($dapan1 != "") echo "<div class='dapan' id='da1' onclick='chkkq(1)'>".$dapan1."<br>"."</div>";
-        if($dapan2 != "") echo "<div class='dapan' id='da2' onclick='chkkq(2)'>".$dapan2."<br>"."</div>";
-        if($dapan3 != "") echo "<div class='dapan' id='da3' onclick='chkkq(3)'>".$dapan3."<br>"."</div>";
-        if($dapan4 != "") echo "<div class='dapan' id='da4' onclick='chkkq(4)'>".$dapan4."<br>"."</div>";
-        
+        if($dapan1 != "") echo "<div class='dapan' id='da1' onclick='chkkq(1,".$cau.")'>".$dapan1."<br>"."</div>";
+        if($dapan2 != "") echo "<div class='dapan' id='da2' onclick='chkkq(2,".$cau.")'>".$dapan2."<br>"."</div>";
+        if($dapan3 != "") echo "<div class='dapan' id='da3' onclick='chkkq(3,".$cau.")'>".$dapan3."<br>"."</div>";
+        if($dapan4 != "") echo "<div class='dapan' id='da4' onclick='chkkq(4,".$cau.")'>".$dapan4."<br>"."</div>";
         ?>
 
-        <button name="cau" value="<?php echo $cau-1?>" <?php if($cau == 1) echo "disabled"?>>previous</button>
+        <button name="cau" value="<?php echo $cau-1?>" <?php if($cau == 1) echo "disabled"?> <?php ?>>previous</button>
         <button name="cau" value="<?php echo $cau+1?>" <?php if($cau == 600) echo "disabled"?>>Next</button>
         
         <div>
@@ -78,22 +74,23 @@
 </form>
     <!-- Script -->
     <script>
-        function chkkq(kq, vtda){
+        function chkkq(kq, cau){
             socau = <?php if($dapan4 != "") echo 5; else echo 4;?>;
             if(kq == <?php echo $dapandung ?>){
                 document.getElementById("da" + kq).style.backgroundColor = "lightgreen";
                 document.getElementById("da" + kq).style.border = "4px solid green";
-
+                document.getElementById("btn" + cau).style.backgroundColor = "green";
                for(var i = 1; i < socau; i++){
                     if(i != <?php echo $dapandung?>){
                         document.getElementById("da" + i).style.backgroundColor = "aquamarine";
                         document.getElementById("da" + i).style.border = "none";
                     }
                }
+               
             }else{
                 document.getElementById("da" + kq).style.backgroundColor = "antiquewhite";
                 document.getElementById("da" + kq).style.border = "4px solid red";
-
+                document.getElementById("btn" + cau).style.backgroundColor = "red";
                 for(var i = 1; i < socau; i++){
                     if(i != kq){
                         document.getElementById("da" + i).style.backgroundColor = "aquamarine";
@@ -102,10 +99,13 @@
                }
             }
 
-            if(kq == <?php echo $dapandung ?>)
+            if(kq == <?php echo $dapandung ?>){
                 document.getElementById("da").innerHTML = "Chính xác";
+                return true;
+            }
             else{
                 document.getElementById("da").innerHTML = "Đáp án chưa chính xác";
+                return false;
             }
         }
     </script>

2c5e02192632b29a4b1b9bc766f21912606c928e
NTPhus <phu444080@gmail.com>
2024-05-30 21:30:03 +0700

    Hehe

--- trangchu.php
+++ trangchu.php
@@ -326,7 +326,7 @@
         if(trang == "OnTapMoPhong"){
             window.location.href = "giaoDienMoPhong.php";
         }else if(trang == "ThiMoPhong"){
-            window.location.href = "giaoDienThiMP.php";
+            window.location.href = "giaoDienMoPhong.php";
         }else if(trang == "OnLyThuyet"){
             window.location.href = "giaoDienTracNghiem.php";
         }else if(trang == "ThiLyThuyet"){
